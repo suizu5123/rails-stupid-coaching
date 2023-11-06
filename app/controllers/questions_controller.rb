15b2409798e6b6b9b0180ef53c8c7c4cb83138c8
Scott Stevens <suizu5123@gmail.com>
2023-11-06 12:13:03 +0900

    Got first test to work, will return + finish styling for ask page later

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    if params[:answer]
+      if params[:answer] == "I am going to work"
+        @coach_answer = "Great!"
+      elsif params[:answer][-1,1] == "?"
+        @coach_answer = "Silly question, get dressed and go to work!"
+      else
+        @coach_answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
+end
